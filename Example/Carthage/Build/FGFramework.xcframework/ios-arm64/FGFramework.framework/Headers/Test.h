d8726ee44ba8fa824c85bd96d8e8020dfeb81fa8
jinyongfeng <15757127193@163.com>
2021-10-28 16:03:22 +0800

    update

---
+++ Example/Carthage/Build/FGFramework.xcframework/ios-arm64/FGFramework.framework/Headers/Test.h
@@ -0,0 +1,16 @@
+//
+//  Test.h
+//  FGShortVideoKit
+//
+//  Created by FoneG on 2021/10/28.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Test : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/Carthage/Build/FGFramework.xcframework/ios-arm64_x86_64-simulator/FGFramework.framework/Headers/FGFramework.h
@@ -0,0 +1,19 @@
+//
+//  FGFramework.h
+//  FGFramework
+//
+//  Created by FoneG on 2021/10/28.
+//  Copyright © 2021 15757127193@163.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FGFramework.
+FOUNDATION_EXPORT double FGFrameworkVersionNumber;
+
+//! Project version string for FGFramework.
+FOUNDATION_EXPORT const unsigned char FGFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FGFramework/PublicHeader.h>
+
+
